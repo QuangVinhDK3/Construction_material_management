a519baacd3dec256e174f31b64ebcd2a721245dd
QuangVinh <tienphatxsolp1777@gmail.com>
2026-04-20 11:37:31 +0700

    QuangVinh(Leader): Completed: Setup project,UI Slide bar,Connection Database, Install the necessary libraries,Font,Icon

---
+++ QuanLyVatLieuXayDung/QuanLyVatLieuXayDung/Services/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLyVatLieuXayDung.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyVatLieuXayDung.Services.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<VatLieu> VatLieus { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Thay đổi Server Name cho đúng với máy
+            // Dùng để kết nối DB
+            optionsBuilder.UseSqlServer("Server=DESKTOP-HP165L4\\SQLEXPRESS;Database=QuanLyVatLieuDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
